b9ee984d4040ce152a5886cf6071cf216a697e0e
anupam <saraswat.anupam@gmail.com>
2014-05-08 17:03:58 -0600

    componenetscan annotation placed.

--- activemqWeb/src/activemqWeb/JMSConfig.java
+++ activemqWeb/src/activemqWeb/JMSConfig.java
@@ -2,9 +2,11 @@ package activemqWeb;
 
 import org.springMQTest.model.Message;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@ComponentScan(basePackages={"org.springMQTest.model"})
 public class JMSConfig {
 	
 	@Bean
